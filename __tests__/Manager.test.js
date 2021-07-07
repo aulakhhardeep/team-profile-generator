9e150a8b44c206bfba536d3b2cedb4759ed405d2
Hardeep <aulakh.hardeep91@gmail.com>
2021-07-07 09:15:15 -0400

    Added tests and methods for Manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -9,3 +9,27 @@ test("creates a manager object", () => {
 	expect(manager.email).toBe("sam@wilson.com");
 	expect(manager.officeNumber).toBe(905);
 });
+
+test("tests the Name method", () => {
+	const manager = new Manager("Sam", 2, "sam@wilson.com", 905);
+
+	expect(manager.getName()).toBe("Sam");
+});
+
+test("tests the Id method", () => {
+	const manager = new Manager("Sam", 2, "sam@wilson.com", 905);
+
+	expect(manager.getId()).toBe(2);
+});
+
+test("tests the Email method", () => {
+	const manager = new Manager("Sam", 2, "sam@wilson.com", 905);
+
+	expect(manager.getEmail()).toBe("sam@wilson.com");
+});
+
+test("tests the Role method", () => {
+	const manager = new Manager("Sam", 2, "sam@wilson.com", 905);
+
+	expect(manager.getRole()).toBe("Manager");
+});
