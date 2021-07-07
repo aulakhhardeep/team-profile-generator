32c35a2359f31ecc8723af78fc42bde6060af348
Hardeep <aulakh.hardeep91@gmail.com>
2021-07-07 08:58:28 -0400

    Added methods and  tests to Employee profile

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -10,3 +10,27 @@ test("creates an employee object", () => {
 	expect(employee.email).toEqual(expect.any(String));
 	expect(employee.email).toEqual("sam@wilson.com");
 });
+
+test("tests the Name method", () => {
+	const employee = new Employee("Sam", 2, "sam@wilson.com");
+
+	expect(employee.getName()).toBe("Sam");
+});
+
+test("tests the Id method", () => {
+	const employee = new Employee("Sam", 2, "sam@wilson.com");
+
+	expect(employee.getId()).toBe(2);
+});
+
+test("tests the Email method", () => {
+	const employee = new Employee("Sam", 2, "sam@wilson.com");
+
+	expect(employee.getEmail()).toBe("sam@wilson.com");
+});
+
+test("tests the Role method", () => {
+	const employee = new Employee("Sam", 2, "sam@wilson.com");
+
+	expect(employee.getRole()).toBe("Employee");
+});
